e30b457f3765a7b9540e7f7e058e6e5b5af6eccd
Ali Raheem <wolfmankurd@users.noreply.github.com>
2017-01-30 19:19:25 +0000

    now version 0.2.0
    Added missing url and description to project file,

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojurine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojurine "0.2.0-SNAPSHOT"
+  :description "anigram finder"
+  :url "https://github.com/wolfmankurd/Clojurine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
